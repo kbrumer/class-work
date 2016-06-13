1ce4fe7f3d02db407d36aa63fb3973b2ea87c9e6
marty <marty@martypdx.com>
2016-06-13 15:26:31 -0700

    cowsay ftw!

--- week6/june_13_basic_webpack_angular/app/src/app.js
+++ week6/june_13_basic_webpack_angular/app/src/app.js
@@ -1,9 +1,17 @@
 const angular = require( 'angular' );
-const greeting = require( './greeting' );
+// const greeting = require( './greeting' );
+const cowsay = require( 'cowsay-browser' );
 require( './css/main.css' );
 
 const app = angular.module( 'app', [] );
 app.controller( 'main', function( $scope ){
-	$scope.place = 'pdx';
-	$scope.greeting = greeting;
+	$scope.text = 'I\'m a moooodule';
+	$scope.method = 'say';
+	$scope.cow = function( method, text ) {
+		return cowsay[method]({
+			text: text || '(I\'m speechless)',
+			e : 'Oo',
+			T : 'U '
+		});
+	};
 });
