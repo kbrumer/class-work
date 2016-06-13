1d44354511730c4e4e72babe6a8f40a2608d3ffa
marty <marty@martypdx.com>
2016-06-13 16:53:44 -0700

    allow choosing type

--- week6/june_13_basic_webpack_angular/app/src/app.js
+++ week6/june_13_basic_webpack_angular/app/src/app.js
@@ -1,17 +1,23 @@
 const angular = require( 'angular' );
 // const greeting = require( './greeting' );
 const cowsay = require( 'cowsay-browser' );
+
 require( './css/main.css' );
+
 
 const app = angular.module( 'app', [] );
 app.controller( 'main', function( $scope ){
+	cowsay.list( ( err, types ) => $scope.types = types );
+	
+	$scope.type = 'default';
 	$scope.text = 'I\'m a moooodule';
 	$scope.method = 'say';
-	$scope.cow = function( method, text ) {
+	$scope.cow = function( method, type, text ) {
 		return cowsay[method]({
 			text: text || '(I\'m speechless)',
 			e : 'Oo',
-			T : 'U '
+			T : 'U ',
+			f : type
 		});
 	};
 });
