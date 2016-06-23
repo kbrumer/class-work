5d77c9df5845ce67426d19998015c7075e03902c
marty <marty@martypdx.com>
2016-06-23 12:17:00 -0700

    update regex to correctly exclude ./index.js

--- week7/june_23_sass_moar_components/app/src/components/index.js
+++ week7/june_23_sass_moar_components/app/src/components/index.js
@@ -5,13 +5,12 @@ import path from 'path';
 const reqContext = require.context(
 	'./', 
 	true, 
-	/.js$/
+	/^\.\/(?!index).+?\.js$/
 );
 
 const components = angular.module( 'components', [] );
 
 reqContext.keys().forEach( key => {
-	if ( key.startsWith( './index' ) ) return;
 	const name = camelcase( path.basename( key, '.js' ) );
 	components.component( name, reqContext( key ).default );
 });
