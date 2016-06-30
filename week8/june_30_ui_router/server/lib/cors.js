6653077532764dce81aaeb0ae41fc28228c24219
marty <marty@martypdx.com>
2016-06-30 11:05:56 -0700

    server for ui-router

---
+++ week8/june_30_ui_router/server/lib/cors.js
@@ -0,0 +1,8 @@
+module.exports = function cors( url ){
+	return ( req, res, next ) => {
+		res.header( 'Access-Control-Allow-Origin', url );
+		res.header( 'Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE' );
+		res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' );
+		next();
+	};
+};
